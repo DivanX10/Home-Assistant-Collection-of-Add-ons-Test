adf3f43896605a890adf10223c6e1b97adc2a1e6
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-09-03 20:40:34 +0300

    Update run.sh

--- deepstack-trainer/run.sh
+++ deepstack-trainer/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-values=`cat /data/options.json`
-for s in $(echo $values | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
-    export $s
-done
-
 mkdir -p /config/deepstack/deepstack-trainer
 cp -r /config/deepstack/deepstack-trainer/ /opt/trainer/db
 cp -r /config/deepstack/deepstack-trainer/ /opt/trainer/photos/uploads
