7b7ad4051a97a2fadd8afd6180ea07238e29dacb
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-09-03 20:48:35 +0300

    Delete run.sh

--- deepstack-trainer/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /config/deepstack/deepstack-trainer
-cp -r /config/deepstack/deepstack-trainer/ /opt/trainer/db
-cp -r /config/deepstack/deepstack-trainer/ /opt/trainer/photos/uploads
-cp -r /opt/trainer/db /config/deepstack/deepstack-trainer/
-cp -r /opt/trainer/photos/uploads /config/deepstack/deepstack-trainer/
