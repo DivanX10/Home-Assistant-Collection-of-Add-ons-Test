d9c344270412f8c3fe1feebf37156bab04dc4e0f
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-09-04 20:49:56 +0300

    Update run.sh

--- deepstack-trainer-debian/run.sh
+++ deepstack-trainer-debian/run.sh
@@ -2,13 +2,4 @@
 set -e
 
 exec /usr/bin/python3 trainer.py &
-exec mkdir -p /config/deepstack/deepstack-trainer/db &
-exec mkdir -p /config/deepstack/deepstack-trainer/uploads &
-
-#copy from the Home Assistant to the container
-exec cp -r /config/deepstack/deepstack-trainer/db/* /opt/trainer/db/ &
-exec cp -r /config/deepstack/deepstack-trainer/uploads/* /opt/trainer/photos/uploads/ &
-
-#copy from the container to the Home Assistant
-exec cp -r /opt/trainer/db/ /config/deepstack/deepstack-trainer/ &
-exec cp -r /opt/trainer/photos/uploads/* /config/deepstack/deepstack-trainer/uploads/ &
+exec /bin/sh -c /opt/trainer/backup.sh
