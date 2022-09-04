d3aeacc19e94991ce1f8b4a94e12a5e1aea76b1c
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-09-04 20:47:47 +0300

    Update run.sh

--- deepstack-trainer-debian/run.sh
+++ deepstack-trainer-debian/run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e
 
-exec /usr/bin/python3 trainer.py &&
-
-#creating a folder
-mkdir -p /config/deepstack/deepstack-trainer/db &&
-mkdir -p /config/deepstack/deepstack-trainer/uploads &&
+exec /usr/bin/python3 trainer.py &
+exec mkdir -p /config/deepstack/deepstack-trainer/db &
+exec mkdir -p /config/deepstack/deepstack-trainer/uploads &
 
 #copy from the Home Assistant to the container
-cp -r /config/deepstack/deepstack-trainer/db/* /opt/trainer/db/ &&
-cp -r /config/deepstack/deepstack-trainer/uploads/* /opt/trainer/photos/uploads/ &&
+exec cp -r /config/deepstack/deepstack-trainer/db/* /opt/trainer/db/ &
+exec cp -r /config/deepstack/deepstack-trainer/uploads/* /opt/trainer/photos/uploads/ &
 
 #copy from the container to the Home Assistant
-cp -r /opt/trainer/db/ /config/deepstack/deepstack-trainer/ &&
-cp -r /opt/trainer/photos/uploads/* /config/deepstack/deepstack-trainer/uploads/ &&
+exec cp -r /opt/trainer/db/ /config/deepstack/deepstack-trainer/ &
+exec cp -r /opt/trainer/photos/uploads/* /config/deepstack/deepstack-trainer/uploads/ &
