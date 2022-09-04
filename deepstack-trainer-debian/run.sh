5b363d609b2340790c943ece28b6155510e2a3d3
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-09-04 20:33:45 +0300

    Add files via upload

---
+++ deepstack-trainer-debian/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#creating a folder
+mkdir -p /config/deepstack/deepstack-trainer &&
+
+#copy from the Home Assistant to the container
+cp -r /config/deepstack/deepstack-trainer/db/* /opt/trainer/db &&
+cp -r /config/deepstack/deepstack-trainer/uploads/* /opt/trainer/photos/uploads &&
+
+#copy from the container to the Home Assistant
+cp -r /opt/trainer/db /config/deepstack/deepstack-trainer/ &&
+cp -r /opt/trainer/photos/uploads/* /config/deepstack/deepstack-trainer/
