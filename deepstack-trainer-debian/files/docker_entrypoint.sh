9ea63193fc379186640a2886d5a5f735745722c9
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-09-04 23:13:50 +0300

    Update docker_entrypoint.sh

--- deepstack-trainer-debian/files/docker_entrypoint.sh
+++ deepstack-trainer-debian/files/docker_entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 #exec /usr/bin/python3 trainer.py &
 #exec /usr/bin/python3 run.py
 
-exec python3 trainer.py &
-exec python3 run.py
+#exec python3 trainer.py &
+#exec python3 run.py
 
-#python3 trainer.py &
-#python3 run.py
+python3 trainer.py &
+python3 run.py
