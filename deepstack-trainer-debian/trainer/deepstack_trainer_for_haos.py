2b9d9a844ae289f26cb681e6b0dadbc098ba76b4
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-09-06 02:21:29 +0300

    Update deepstack_trainer_for_haos.py

--- deepstack-trainer-debian/trainer/deepstack_trainer_for_haos.py
+++ deepstack-trainer-debian/trainer/deepstack_trainer_for_haos.py
@@ -106,8 +106,7 @@ def SaveImage(file, path):
             shutil.copyfileobj(file, buffer)
             shutil.copy(src_file_images_db, dest_file_db) #copy the database from docker to homeassistant
             shutil.copytree(src_file_photos, dest_file_photos, dirs_exist_ok=True) #copy the photos from docker to homeassistant
-        logger.info("File saved Divan")
-        logger.info("Double message File saved Divan")
+        logger.info("File saved")
     except Exception as e:
         logger.error("Unable to save file " + str(e))
         raise Exception(str(e))
