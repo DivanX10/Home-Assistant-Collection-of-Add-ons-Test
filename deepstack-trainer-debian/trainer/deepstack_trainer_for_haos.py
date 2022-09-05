fe53cb50208400074722e3a311e530088089a7da
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-09-05 05:45:53 +0300

    Update deepstack_trainer_for_haos.py

--- deepstack-trainer-debian/trainer/deepstack_trainer_for_haos.py
+++ deepstack-trainer-debian/trainer/deepstack_trainer_for_haos.py
@@ -17,8 +17,8 @@ deepstack_api_key = os.getenv("DEEPSTACK_API_KEY")
 min_confidence = os.getenv("MIN_CONFIDANCE")
 
 #copy the database from docker to homeassistant
-#src_file_db='/opt/trainer/db'
-#dest_file_db = os.getenv("HOMEASSISTANT_FOLDER_PATH_FOR_DATABASE")
+src_file_db='/opt/trainer/db'
+dest_file_db = os.getenv("HOMEASSISTANT_FOLDER_PATH_FOR_DATABASE")
 
 #copy the photos from docker to homeassistant
 src_file_photos= '/opt/trainer/photos/uploads'
@@ -86,18 +86,21 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in "jpg,png,gif,bmp,jpeg"
 
+################################################################################
+#Это абзац отвечает за копирование файлов
 def SaveImage(file, path):
     logger.info("Saving the image to the file system")
     try:
         with open(path, "wb") as buffer:
             shutil.copyfileobj(file, buffer)
-            #shutil.copytree(src_file_db, dest_file_db) #copy the database from docker to homeassistant
+            shutil.copytree(src_file_db, dest_file_db) #copy the database from docker to homeassistant
             shutil.copytree(src_file_photos, dest_file_photos) #copy the photos from docker to homeassistant
         logger.info("File saved Divan")
         logger.info("Double message File saved Divan")
     except Exception as e:
         logger.error("Unable to save file " + str(e))
         raise Exception(str(e))
+################################################################################        
 
 def convertToBinaryData(filename):
     logger.info("Converting To Binary Data: " + filename)
