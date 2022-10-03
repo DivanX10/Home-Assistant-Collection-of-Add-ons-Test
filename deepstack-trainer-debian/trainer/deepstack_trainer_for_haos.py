d04b4de6c09eac6047fe8b2c6a67b4710044eda7
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2022-10-04 02:49:47 +0300

    Update deepstack_trainer_for_haos.py

--- deepstack-trainer-debian/trainer/deepstack_trainer_for_haos.py
+++ deepstack-trainer-debian/trainer/deepstack_trainer_for_haos.py
@@ -356,6 +356,6 @@ def get_images(request: Request):
 if __name__ == '__main__':
     InitDB()
     uvicorn.run(app, host="0.0.0.0", port=8080)
-    os.system('cp -r /config/deepstack/db/* /opt/trainer/db') #копируем базу из /config/deepstack/db в /opt/trainer/
-    os.system('cp -r /config/deepstack/photos/* /opt/trainer/photos/uploads') #копируем фото из /config/deepstack/photos в /opt/trainer/photos/uploads/
+    os.system('cp -r /config/deepstack/db/* /opt/trainer/db')
+    os.system('cp -r /config/deepstack/photos/* /opt/trainer/photos/uploads')
 
