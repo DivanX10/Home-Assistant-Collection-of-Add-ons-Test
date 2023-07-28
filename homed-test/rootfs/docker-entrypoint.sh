6be89be7790b342d03de51dd75dc8da41c395890
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-07-28 21:56:03 +0300

    Update docker-entrypoint.sh

--- homed-test/rootfs/docker-entrypoint.sh
+++ homed-test/rootfs/docker-entrypoint.sh
@@ -5,10 +5,10 @@ bashio::log.info "Preparing to start..."
 bashio::config.require 'data_path'
 
 export HOMED_DATA="$(bashio::config 'data_path')"
-if ! bashio::fs.file_exists "$HOMED_DATA/configuration.yaml"; then
+if ! bashio::fs.file_exists "$HOMED_DATA/homed-zigbee.yaml"; then
     mkdir -p "$HOMED_DATA" || bashio::exit.nok "Could not create $HOMED_DATA"
 
-    cat <<EOF > "$HOMED_DATA/configuration.yaml"
+    cat <<EOF > "$HOMED_DATA/homed-zigbee.yaml"
 homeassistant: true
 EOF
 fi
