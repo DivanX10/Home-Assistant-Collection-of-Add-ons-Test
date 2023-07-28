14e71d7b134e42b3add5920099a7d14f738ab1a4
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-07-29 02:19:42 +0300

    Update docker-entrypoint.sh

--- homed-test/rootfs/docker-entrypoint.sh
+++ homed-test/rootfs/docker-entrypoint.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/with-contenv bash
 
-touch /etc/homed/homed-zigbee.conf
-
 # Путь к файлу homed-zigbee.conf внутри контейнера
 DOCKER_HOMED_CONF="/etc/homed/homed-zigbee.conf"
 
 # Путь к файлу homed-zigbee.conf на хосте (папка config/homed)
 HOST_HOMED_CONF="/config/homed/homed-zigbee.conf"
 
-# Копируем файл, если он существует на хосте
+# Проверяем, существует ли файл homed-zigbee.conf на хосте
 if [ -f "$HOST_HOMED_CONF" ]; then
-    cp -p "$HOST_HOMED_CONF" "$DOCKER_HOMED_CONF"
+    # Копируем содержимое файла на хосте в новый файл внутри контейнера
+    cat "$HOST_HOMED_CONF" > "$DOCKER_HOMED_CONF"
+else
+    # Создаем пустой файл homed-zigbee.conf, если его нет на хосте
+    touch "$DOCKER_HOMED_CONF"
+    # Копируем содержимое файла на хосте в новый файл внутри контейнера
+    cat "$HOST_HOMED_CONF" > "$DOCKER_HOMED_CONF"
 fi
 
-echo "Done copy config Homed-zigbee"
+echo "Done setting up Homed-zigbee configuration."
+
 
 
