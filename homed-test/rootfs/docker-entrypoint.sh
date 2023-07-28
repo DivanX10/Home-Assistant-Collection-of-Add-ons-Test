5ce11bc545d8e532ceb8b9544c94fb40b827e180
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-07-29 02:51:24 +0300

    Update docker-entrypoint.sh

--- homed-test/rootfs/docker-entrypoint.sh
+++ homed-test/rootfs/docker-entrypoint.sh
@@ -3,24 +3,13 @@
 # Путь к файлу homed-zigbee.conf внутри контейнера
 DOCKER_HOMED_CONF="/etc/homed/homed-zigbee.conf"
 
-# Путь к файлу homed-zigbee.conf на хосте (папка config/homed)
-HOST_HOMED_CONF="/config/homed/homed-zigbee.conf"
+# Преобразуем относительный путь в абсолютный с помощью realpath
+CONFIG_PATH_ABSOLUTE=$(realpath "$CONFIG_PATH")
 
-# Проверяем, существует ли файл homed-zigbee.conf на хосте
-if [ -f "$HOST_HOMED_CONF" ]; then
-    # Копируем содержимое файла на хосте в новый файл внутри контейнера
-    cat "$HOST_HOMED_CONF" > "$DOCKER_HOMED_CONF"
-else
-    # Создаем пустой файл homed-zigbee.conf, если его нет на хосте
-    touch "$DOCKER_HOMED_CONF"
-    # Копируем содержимое файла на хосте в новый файл внутри контейнера
-    cat "$HOST_HOMED_CONF" > "$DOCKER_HOMED_CONF"
-fi
+# Копируем содержимое файла из переменной окружения CONFIG_PATH_ABSOLUTE в новый файл внутри контейнера
+cat "$CONFIG_PATH_ABSOLUTE/homed-zigbee.conf" > "$DOCKER_HOMED_CONF"
 
 echo "Done setting up Homed-zigbee configuration."
 
 # Вечный процесс для предотвращения завершения контейнера
 tail -f /dev/null
-
-
-
