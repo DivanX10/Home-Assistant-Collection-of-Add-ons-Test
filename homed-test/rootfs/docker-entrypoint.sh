c3f718441d0a43e6d0f7d8c487de4b167e25282e
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-07-29 03:00:16 +0300

    Update docker-entrypoint.sh

--- homed-test/rootfs/docker-entrypoint.sh
+++ homed-test/rootfs/docker-entrypoint.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/with-contenv bash
 
-# Путь к файлу homed-zigbee.conf внутри контейнера
-DOCKER_HOMED_CONF="/etc/homed/homed-zigbee.conf"
+# Функция для создания homed-zigbee.conf
+create_homed_config() {
+    local config_path="$1"
+
+    # Проверяем, существует ли файл homed-zigbee.conf по указанному пути
+    if [ -f "$config_path/homed-zigbee.conf" ]; then
+        # Путь к файлу homed-zigbee.conf внутри контейнера
+        local docker_homed_conf="/etc/homed/homed-zigbee.conf"
+
+        # Копируем содержимое файла из указанного пути внутри контейнера
+        cat "$config_path/homed-zigbee.conf" > "$docker_homed_conf"
+        echo "Done setting up Homed-zigbee configuration."
+    else
+        echo "Error: File homed-zigbee.conf not found at path: $config_path"
+    fi
+}
 
 # Проверяем, существует ли переменная data_path и не пустая ли она
 if [ -n "$data_path" ]; then
     # Преобразуем относительный путь в абсолютный с помощью realpath
-    CONFIG_PATH_ABSOLUTE=$(realpath "$data_path")
-
-    # Проверяем, существует ли файл homed-zigbee.conf по указанному пути
-    if [ -f "$CONFIG_PATH_ABSOLUTE/homed-zigbee.conf" ]; then
-        # Копируем содержимое файла из переменной окружения CONFIG_PATH_ABSOLUTE в новый файл внутри контейнера
-        cat "$CONFIG_PATH_ABSOLUTE/homed-zigbee.conf" > "$DOCKER_HOMED_CONF"
-        echo "Done setting up Homed-zigbee configuration."
-    else
-        echo "Error: File homed-zigbee.conf not found at path: $CONFIG_PATH_ABSOLUTE"
-    fi
+    local config_path_absolute=$(realpath "$data_path")
+    create_homed_config "$config_path_absolute"
 else
     echo "Error: 'data_path' is not specified in the addon configuration."
 fi
 
 # Вечный процесс для предотвращения завершения контейнера
 tail -f /dev/null
+
 
