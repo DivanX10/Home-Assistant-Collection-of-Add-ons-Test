b2a45326d48bc242d8dd4fe155591e652f5399b3
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-08-07 04:39:24 +0300

    Update run_agentdvr.sh

--- agentdvr/run_agentdvr.sh
+++ agentdvr/run_agentdvr.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Извлекаем значение "folder_path" из файла options.json
+FOLDER_PATH=$(jq -r '.folder_path' < /data/options.json)
+
 # Путь к папке с файлами внутри контейнера
 DOCKER_MEDIA_DIR="/agent/Media"
 DOCKER_XML_DIR="/agent/Media/XML"
@@ -10,32 +13,31 @@ HOST_MEDIA_DIR="$FOLDER_PATH/Media"
 HOST_XML_DIR="$FOLDER_PATH/Media/XML"
 HOST_COMMANDS_DIR="$FOLDER_PATH/Commands"
 
+# Проверяем, существует ли папка на хосте
+if [ ! -d "$HOST_MEDIA_DIR" ]; then
+    # Создаем папку на хосте
+    mkdir -p "$HOST_MEDIA_DIR"
+fi
+
+if [ ! -d "$HOST_XML_DIR" ]; then
+    # Создаем папку на хосте
+    mkdir -p "$HOST_XML_DIR"
+fi
+
+if [ ! -d "$HOST_COMMANDS_DIR" ]; then
+    # Создаем папку на хосте
+    mkdir -p "$HOST_COMMANDS_DIR"
+fi
+
 # Функция для копирования измененных файлов
 copy_files() {
-    # Проверяем, существует ли папка /agent и она не пуста
-    if [ ! -d "$DOCKER_MEDIA_DIR" ] || [ -z "$(ls -A $DOCKER_MEDIA_DIR)" ]; then
-        # Копируем содержимое папок из контейнера в папки на хосте
-        rsync -avh --ignore-existing "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
-        rsync -avh --ignore-existing "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
-        rsync -avh --ignore-existing "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
-    else
-        # Копируем только измененные файлы из папок на хосте в контейнер
-        rsync -avh --update "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
-        rsync -avh --update "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
-        rsync -avh --update "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
-    fi
+    # Копируем только измененные файлы из папок на хосте в контейнер
+    rsync -avh --update "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
+    rsync -avh --update "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
+    rsync -avh --update "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
 }
 
-# Добавляем задачу в крон
-echo "*/1 * * * * /tmp/run_agentdvr.sh" > cronjob
-
-# Устанавливаем задачу в крон
-crontab cronjob
-
-# Удаляем временный файл
-rm cronjob
-
-# Запускаем скрипт для копирования файлов
+# Копируем файлы перед запуском agentdvr
 copy_files
 
 # Запускаем agentdvr
