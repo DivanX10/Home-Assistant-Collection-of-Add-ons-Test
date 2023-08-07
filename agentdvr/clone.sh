4a7de4308b7fd08213c84bc3b6f894c3a2b65e8d
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-08-07 05:03:13 +0300

    Update clone.sh

--- agentdvr/clone.sh
+++ agentdvr/clone.sh
@@ -3,39 +3,24 @@
 # Извлекаем значение "folder_path" из файла options.json
 FOLDER_PATH=$(jq -r '.folder_path' < /data/options.json)
 
-# Путь к папке с файлами внутри контейнера
-DOCKER_MEDIA_DIR="/agent/Media"
-DOCKER_XML_DIR="/agent/Media/XML"
-DOCKER_COMMANDS_DIR="/agent/Commands"
-
 # Путь к папке с файлами на хосте (папка share/agentdvr)
 HOST_MEDIA_DIR="$FOLDER_PATH/Media"
 HOST_XML_DIR="$FOLDER_PATH/Media/XML"
 HOST_COMMANDS_DIR="$FOLDER_PATH/Commands"
 
-# Проверяем, существует ли папка на хосте
-if [ ! -d "$HOST_MEDIA_DIR" ]; then
-    # Создаем папку на хосте
-    mkdir -p "$HOST_MEDIA_DIR"
-fi
-
-if [ ! -d "$HOST_XML_DIR" ]; then
-    # Создаем папку на хосте
-    mkdir -p "$HOST_XML_DIR"
-fi
-
-if [ ! -d "$HOST_COMMANDS_DIR" ]; then
-    # Создаем папку на хосте
-    mkdir -p "$HOST_COMMANDS_DIR"
-fi
+# Путь к папке с файлами внутри контейнера
+DOCKER_MEDIA_DIR="/agent/Media"
+DOCKER_XML_DIR="/agent/Media/XML"
+DOCKER_COMMANDS_DIR="/agent/Commands"
 
 # Функция для копирования измененных файлов
 copy_files() {
     # Копируем только измененные файлы из папок на хосте в контейнер
-    rsync -avh --update "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
-    rsync -avh --update "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
-    rsync -avh --update "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
+    rsync -avh --update "$DOCKER_MEDIA_DIR" "$HOST_MEDIA_DIR/"
+    rsync -avh --update "$DOCKER_XML_DIR" "$HOST_XML_DIR/"
+    rsync -avh --update "$DOCKER_COMMANDS_DIR" "$HOST_COMMANDS_DIR/"
 }
 
-# Копируем файлы из контейнера в папку на хосте
+# Копируем файлы с папки в Home Assistant в контейнер
 copy_files
+
