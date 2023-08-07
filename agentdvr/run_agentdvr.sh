cf37139d0519bf5f94ec935d8aa2258cc0e9c872
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-08-07 03:42:08 +0300

    Update run_agentdvr.sh

--- agentdvr/run_agentdvr.sh
+++ agentdvr/run_agentdvr.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
-# Создаем папки на хосте, если их нет
-mkdir -p /share/agentdvr/Media
-mkdir -p /share/agentdvr/Media/XML
-mkdir -p /share/agentdvr/Commands
+# Путь к папке с файлами внутри контейнера
+DOCKER_MEDIA_DIR="/agent/Media"
+DOCKER_XML_DIR="/agent/Media/XML"
+DOCKER_COMMANDS_DIR="/agent/Commands"
 
-# Монтируем Docker volumes в папки внутри контейнера
-mount -o bind /share/agentdvr/Media /agent/Media
-mount -o bind /share/agentdvr/Media/XML /agent/Media/XML
-mount -o bind /share/agentdvr/Commands /agent/Commands
+# Путь к папке с файлами на хосте (папка share/agentdvr)
+HOST_MEDIA_DIR="/share/agentdvr/Media"
+HOST_XML_DIR="/share/agentdvr/Media/XML"
+HOST_COMMANDS_DIR="/share/agentdvr/Commands"
+
+# Проверяем, существует ли папка /agent и она не пуста
+if [ ! -d "$DOCKER_MEDIA_DIR" ] || [ -z "$(ls -A $DOCKER_MEDIA_DIR)" ]; then
+    # Копируем содержимое папок из контейнера в папки на хосте
+    rsync -avh --ignore-existing "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
+    rsync -avh --ignore-existing "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
+    rsync -avh --ignore-existing "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
+else
+    # Копируем только измененные файлы из папок на хосте в контейнер
+    rsync -avh --update "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
+    rsync -avh --update "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
+    rsync -avh --update "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
+fi
 
 # Запускаем agentdvr
 exec /agent/Agent
+
+# После завершения работы agentdvr копируем файлы обратно в папку на хосте
+rsync -avh --update "$DOCKER_MEDIA_DIR/" "$HOST_MEDIA_DIR"
+rsync -avh --update "$DOCKER_XML_DIR/" "$HOST_XML_DIR"
+rsync -avh --update "$DOCKER_COMMANDS_DIR/" "$HOST_COMMANDS_DIR"
+
 
 
