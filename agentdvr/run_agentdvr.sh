93e923ae99a1f6a625b5e790ceec1b14fb7b82cf
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-08-07 04:10:15 +0300

    Update run_agentdvr.sh

--- agentdvr/run_agentdvr.sh
+++ agentdvr/run_agentdvr.sh
@@ -29,23 +29,24 @@ if [ ! -d "$HOST_COMMANDS_DIR" ]; then
     mkdir -p "$HOST_COMMANDS_DIR"
 fi
 
-# Проверяем, существует ли папка /agent и она не пуста
-if [ ! -d "$DOCKER_MEDIA_DIR" ] || [ -z "$(ls -A $DOCKER_MEDIA_DIR)" ]; then
-    # Копируем содержимое папок из контейнера в папки на хосте
-    rsync -avh --ignore-existing "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
-    rsync -avh --ignore-existing "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
-    rsync -avh --ignore-existing "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
-else
-    # Копируем только измененные файлы из папок на хосте в контейнер
-    rsync -avh --update "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
-    rsync -avh --update "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
-    rsync -avh --update "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
-fi
+# Функция для копирования файлов
+copy_files() {
+    # Проверяем, существует ли папка /agent и она не пуста
+    if [ ! -d "$DOCKER_MEDIA_DIR" ] || [ -z "$(ls -A $DOCKER_MEDIA_DIR)" ]; then
+        # Копируем содержимое папок из контейнера в папки на хосте
+        rsync -avh --ignore-existing "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
+        rsync -avh --ignore-existing "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
+        rsync -avh --ignore-existing "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
+    else
+        # Копируем только измененные файлы из папок на хосте в контейнер
+        rsync -avh --update "$HOST_MEDIA_DIR/" "$DOCKER_MEDIA_DIR"
+        rsync -avh --update "$HOST_XML_DIR/" "$DOCKER_XML_DIR"
+        rsync -avh --update "$HOST_COMMANDS_DIR/" "$DOCKER_COMMANDS_DIR"
+    fi
+}
 
-# Запускаем agentdvr
-exec /agent/Agent
-
-# После завершения работы agentdvr копируем файлы обратно в папку на хосте
-rsync -avh --update "$DOCKER_MEDIA_DIR/" "$HOST_MEDIA_DIR"
-rsync -avh --update "$DOCKER_XML_DIR/" "$HOST_XML_DIR"
-rsync -avh --update "$DOCKER_COMMANDS_DIR/" "$HOST_COMMANDS_DIR"
+# Запускаем цикл копирования файлов каждые 30 секунд
+while true; do
+    copy_files
+    sleep 30
+done
